a793d17e964362a55bbe7915d8c55670e4dd224d
Ciber-marcelo <marcelones125@gmail.com>
2024-06-30 05:39:07 -0300

    feat: create TitlePage

---
+++ src/components/TitlePage.vue
@@ -0,0 +1,26 @@
+<template>
+      <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+   name: 'TitlePage',
+   props: {
+      msg: {
+         type: String,
+         required: true
+      } 
+   }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+div {
+   color: #000000;
+   font-size: 50px;
+   font-weight: bold;
+   font-family: "Montserrat", sans-serif;
+}
+</style>

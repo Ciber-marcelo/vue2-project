3d0e79fa21948d9b3467892035a48a1b4bda7eef
Ciber-marcelo <marcelones125@gmail.com>
2024-06-30 05:40:50 -0300

    feat: create HomeUsers and DetailsUser

---
+++ src/Views/HomeUsers.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <UserCreate />
+    <ApiData/>
+  </div>
+</template>
+
+<script>
+import ApiData from '@/components/ApiData.vue';
+import UserCreate from '@/components/UserCreate.vue';
+
+export default {
+  name: 'HomeUsers',
+  components: {
+    UserCreate,
+    ApiData,
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 15px;
+}
+</style>

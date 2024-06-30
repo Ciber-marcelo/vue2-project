a4018d6ebe4ee46a80f64ac7d8a2a37a8541e6a3
Ciber-marcelo <marcelones125@gmail.com>
2024-06-30 05:37:56 -0300

    feat: create router

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomeUsers from './Views/HomeUsers.vue';
+import DetailsUser from './Views/DetailsUser.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeUsers
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: DetailsUser
+    }
+  ]
+});

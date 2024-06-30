84ed4eb08d291f56bc3e02cd07bae195c0d88c19
Ciber-marcelo <marcelones125@gmail.com>
2024-06-30 11:49:38 -0300

    chore: edit DetailsUser

--- src/Views/DetailsUser.vue
+++ src/Views/DetailsUser.vue
@@ -1,33 +1,155 @@
 <template>
-   <div>
-      batata2
-   </div>
- </template>
- 
- <script>
- export default {
-   name: 'DetailsUser',
-   // props: {
-   //   msg: String
-   // }
- }
- </script>
- 
- <!-- Add "scoped" attribute to limit CSS to this component only -->
- <style scoped>
- h3 {
-   margin: 40px 0 0;
- }
- ul {
-   list-style-type: none;
-   padding: 0;
- }
- li {
-   display: inline-block;
-   margin: 0 10px;
- }
- a {
-   color: #42b983;
- }
- </style>
- 
+  <div class="details">
+    <div class="container1">
+      <TitlePage :msg='"#" + user.id' />
+      <router-link to="/">
+        <button class="btn">Voltar</button>
+      </router-link>
+    </div>
+
+    <div class="container2">
+      <img :src='user.avatar' alt="img" class="image">
+      <div class="containerText">
+        <div class="subText">Primero nome</div>
+        <div class="text">{{ user.first_name }}</div>
+      </div>
+      <div class="containerText">
+        <div class="subText">Último nome</div>
+        <div class="text">{{ user.last_name }}</div>
+      </div>
+    </div>
+
+    <div class="container3">
+      <div class="containerText">
+        <div class="subText">Endereço de email</div>
+        <div class="text">{{ user.email }}</div>
+      </div>
+      <div class="containerText">
+        <div class="subText">Link de avatar</div>
+        <div class="text">{{ user.avatar }}</div>
+      </div>
+      <div class="containerText">
+        <div class="subText">Link de suporte</div>
+        <div class="text">{{ support.url }}</div>
+      </div>
+      <div class="containerText">
+        <div class="subText">Descrição do usuário</div>
+        <div class="text">{{ support.text }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TitlePage from '@/components/TitlePage.vue';
+import axios from 'axios';
+
+export default {
+  name: 'DetailsUser',
+  components: {
+    TitlePage
+  },
+  props: ['id'],
+
+  data() {
+    return {
+      user: '',
+      support: '',
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get(`https://reqres.in/api/users/${this.id}`);
+      this.user = response.data.data;
+      this.support = response.data.support;
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+.details {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 95px;
+}
+
+.container1 {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 588px;
+  margin-bottom: 42px;
+}
+
+.container2 {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 34px;
+  width: 100%;
+  max-width: 588px;
+  margin-bottom: 30px;
+}
+
+.container2 img {
+  width: 120px;
+  height: 120px;
+  border-radius: 5px;
+}
+
+.container3 {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+  max-width: 588px;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+.containerText {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 7px;
+  font-family: "Montserrat", sans-serif;
+  overflow: hidden;
+
+  @media (max-width: 400px) {
+    align-items: center;
+  }
+}
+
+.subText {
+  font-size: 14px;
+  color: #797979;
+}
+
+.text {
+  font-weight: 600;
+}
+
+.btn {
+  width: 185px;
+  height: 55px;
+  background-color: #F7F7F7;
+  border: none;
+  border-radius: 5px;
+  color: #000000;
+  font-size: 15px;
+  font-weight: 600;
+  font-family: "Montserrat", sans-serif;
+}
+
+.btn:hover {
+  background-color: #d8d8d8;
+}
+</style>
